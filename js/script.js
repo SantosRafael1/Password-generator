b647c4418fd55b9d2589aa6f4315d9c47a2139c9
Rafael Santos <contactrafaelsantos@gmail.com>
2023-01-15 11:32:41 -0300

    Check if the password is less than 16 characters

--- js/script.js
+++ js/script.js
@@ -4,8 +4,16 @@ function generatePassword(event) {
     let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!#$%&*+-=?@^_";
     let password = "";
 
+    //check if the password is less than 16 characters
+    if (passwordLength <= 16) {
+        document.getElementById("password").value = "Password too weak. Select more than 16 characters.";
+        document.getElementById("password").disabled = true;
+        return;
+    }
+
     for (let i = 0; i < passwordLength; i++) {
         password += characters.charAt(Math.floor(Math.random() * characters.length));
+        document.getElementById("password").disabled = false;
     }
 
     document.getElementById("password").value = password;
